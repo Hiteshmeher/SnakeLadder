342ac72e22c19cfb8f2b905b2a865a53b5210db1
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-21 16:15:26 +0530

    [Hitesh]: Player position in game

---
+++ src/com/bridgelabz/snakeladder/Play.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.snakeladder;
+/**
+ * 
+ * @author LENOVO
+ *
+ */
+public class Play {
+	
+	public void position() {
+		/**
+		 * initialize position is 0
+		 */
+		int playerPosition = 0;
+		System.out.println("Players starting Position is " + playerPosition);
+	}
+}
---
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.snakeladder;
+/**
+ * 
+ * @author LENOVO
+ *
+ */
+public class SnakeLadder {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Creating Object of class Play
+		 * calling method
+		 */
+		System.out.println("Welcome to Snake Ladder Game");
+		Play P = new Play();
+		P.position();
+	}
+}
